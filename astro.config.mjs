6f1b267c6a8e181c3b010433a89b9c7518f0b15d
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2025-09-30 21:14:19 -0300

    feat: add Preact support and new components
    - Added @astrojs/preact and preact dependencies to package.json.
    - Created Greeting component for displaying random messages.
    - Updated Header component to include ThemeIcon for theme toggling.
    - Implemented ThemeIcon component with dark/light mode functionality.
    - Modified about page to reflect a finished state.
    - Integrated Greeting component into the home page.
    - Enhanced global styles for dark mode support.
    - Updated tsconfig to use Preact's JSX syntax.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-blog-2025.netlify.app"
+  site: "https://astro-blog-2025.netlify.app",
+  integrations: [preact()]
 });
