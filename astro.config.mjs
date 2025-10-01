63037ae30e5f33b45f26369a925dfea007b361d5
Cleber Ribeiro <cleber.ramos.ribeiro@gmail.com>
2025-09-30 22:04:22 -0300

    fix: ensure newline at end of file in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-2025.netlify.app",
-  integrations: [preact()]
+  integrations: [preact()],
 });
